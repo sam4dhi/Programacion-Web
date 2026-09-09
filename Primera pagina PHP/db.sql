6b56d8a49fbe42780167e5a174182138d4b03d3c
sam4adhi <samadhi.orozzco@gmail.com>
2026-09-09 13:00:39 -0600

    creacion de tabla usuarios

--- Primera pagina PHP/db.sql
+++ Primera pagina PHP/db.sql
@@ -1,2 +1,13 @@
 -- Crear base de datos
 CREATE DATABASE crud_app;
+
+--Sleeccionar la base de datos
+USE crud_app;
+
+CREATE TABLE usuarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefono VARCHAR(15) NOT NULL
+);
+
